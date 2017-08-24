a7cf872af3835643b3dabbebf196bc1bf2b9de1a
Angela Gasparyan <a.gasparyan.93@gmail.com>
2017-08-24 19:31:23 +0400

    git commit

--- lib/base.rb
+++ lib/base.rb
@@ -2,26 +2,90 @@ require "json"
 
 class Base
   attr_accessor :id
-
-  def self.inherited(child)
-    if Dir.exist?("data/#{child.name.downcase}")
-     child.count = Dir.glob("data/#{child.name.downcase}/*.json").size
-    else
-      Dir.mkdir("data/#{child.name.downcase}")
-      child.count = 0
-    end
-  end
   
-  def self.count=(count)
-    @count = count
+  class << self
+
+    def inherited(child)
+      if Dir.exist?("data/#{child.name.downcase}")
+        child.count = Dir.glob("data/#{child.name.downcase}/*.json").size
+      else
+        Dir.mkdir("data/#{child.name.downcase}")
+        child.count = 0
+      end
+    end
+
+    def count=(count)
+      @count = count
+
+    end
+
+    def next_id
+      @count += 1
+    end
+
+    def array_id
+      Dir.glob("data/user/*.json").map do |user|
+        user.split("/").last.split(".").first.to_i
+      end
+    end
+
+    def all
+      Dir.glob(file_path('*')).map do |file_name|
+        from_file file_name
+      end
+    end
+
+    def from_hash(hash)
+      new hash
+    end
+
+    def from_json(json_string)
+      from_hash JSON.parse(json_string)
+    end
+
+    def from_file(file_name)
+      from_json File.read file_name
+    end
+
+    def search_by_id(id)
+      if File.exist? file_path(id)
+        from_file file_path(id)
+      else
+        nil
+      end
+    end
+
+    def where(hash)
+      a = []
+      all.each do |o|
+        valid = true
+        hash.each do |k,v|
+          if  o.send(k) != v
+            valid = false
+            break
+          end
+        end
+        a.push o if valid
+      end
+      a
+    end
+
+    def file_path(id)
+      "data/#{self.name.downcase}/#{id}.json"
+    end
+
+    def create(hash)
+     o = new hash
+     o.save
+     o
+    end
+    
   end
 
-  def self.next_id
-    @count += 1
-  end
+
 
   def initialize(hash = nil)
-   @id = self.class.next_id  
+   @id = self.class.next_id
    self.update hash
   end
 
@@ -36,15 +100,22 @@ class Base
   def to_json
     JSON.generate to_hash
   end
-   
-  def self.from_json(json_string)
-   from_hash JSON.parse(json_string)
+
+  def to_s
+    s = "#{self.class}("
+    self.to_hash.each do |k,v|
+      if v.is_a? String
+        s += "#{k}: \"#{v}\", "
+      else
+        s += "#{k}: #{v}, "
+      end
+    end
+    s.gsub /..$/, ')'
   end
+  alias :inspect :to_s
 
   def update(hash)
-    unless  hash.kind_of? Hash
-      return self
-    end
+    return self unless  hash.kind_of? Hash
     hash.each do |k,v|
       if self.respond_to?("#{k}=")
         self.send "#{k}=", v
@@ -53,26 +124,10 @@ class Base
     self
   end
 
-  def self.from_hash(hash)
-    new.update hash
-  end
-
   def save
     File.write self.class.file_path(id), to_json
   end
 
-  def self.search_by_id(id)
-    if File.exist? file_path(id)
-      from_json File.read file_path(id)
-    else
-      nil
-    end
-
-  end
-
-  def self.file_path(id)
-    "data/#{self.name.downcase}/#{id}.json"
-  end
 
 
 
