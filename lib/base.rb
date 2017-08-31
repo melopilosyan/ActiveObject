1733819d0f7d828e4428cfd9207e7e0513429636
Angela Gasparyan <a.gasparyan.93@gmail.com>
2017-08-31 15:48:41 +0400

    Add app.rb

--- lib/base.rb
+++ lib/base.rb
@@ -3,7 +3,7 @@ require_relative "utils"
 class Base
   include Utils
   attr_accessor :id
-
+  
   class << self
 
     def inherited(child)
@@ -23,7 +23,7 @@ class Base
       @count += 1
     end
 
-    def array_id
+    def ids_in_array
       Dir.glob("data/user/*.json").map do |user|
         user.split("/").last.split(".").first.to_i
       end
@@ -82,78 +82,94 @@ class Base
 
     def field(field_name, field_type)
       define_method("#{field_name}=") do |param|
-     raise TypeError, "Expected #{field_type.to_s.capitalize} given #{param.class}" if param.class.name.downcase != field_type.to_s
+        raise TypeError, "Expected #{field_type.to_s.capitalize} given #{param.class}" if param.class.name.downcase != field_type.to_s
         instance_variable_set "@#{field_name}", param
       end
       attr_reader field_name
     end
-    
+
     def belongs_to(class_name)
-       if class_name.class?
-         define_method(class_name) do
-           klass = Module.const_get(class_name.to_s.classify)
-           klass.search_by_id("#{class_name}_id".to_sym)
-         end
-   
-       end
+        define_method(class_name) do
+          klass = Module.const_get(class_name.to_s.capitalize)
+        klass.search_by_id send("#{class_name}_id") 
+        end
     end
 
     def has_many(class_name)
-      if class_name.class?
-        define_method(class_name) do
-         klass = Module.const_get(class_name.to_s.classify)
-         klass.where "#{self.class.name.downcase}_id".to_sym => id
-        end
+      define_method(class_name) do
+        klass = Module.const_get(class_name.to_s.classify)
+        klass.where "#{self.class.name.downcase}_id".to_sym => id
+      end
+      #raise NameError, "Given #{class_name} expected existing class name"
+    end
+
+    def bef_des_func
+       @bef_des_func || []
+    end
+
+    def before_destroy(func_names)
+      @bef_des_func = [] if @bef_des_func.nil?
+      @bef_des_func.push func_names
+    end
+
+  end
+
+
+  def initialize(hash = nil)
+    @id = self.class.next_id
+    self.update hash
+  end
+
+
+  def to_hash
+    hash = {}
+    self.instance_variables.each do |var|
+      hash[var.to_s.delete('@').to_sym] = self.instance_variable_get var
+    end
+    hash
+  end
+
+  def to_json
+    JSON.generate to_hash
+  end
+
+  def to_s
+    s = "#{self.class}("
+    self.to_hash.each do |k,v|
+      if v.is_a? String
+        s += "#{k}: \"#{v}\", "
       else
-        raise NameError, "Given #{class_name} expected existing class name"
+        s += "#{k}: #{v}, "
       end
     end
+    s.gsub /..$/, ')'
+  end
+  alias :inspect :to_s
 
+  def update(hash)
+    return self unless  hash.kind_of? Hash
+    hash.each do |k,v|
+      if self.respond_to?("#{k}=")
+        self.send "#{k}=", v
+      end
+    end
+    self
   end
 
-
-    def initialize(hash = nil)
-      @id = self.class.next_id
-      self.update hash
-    end
-
-
-    def to_hash
-      hash = {}
-      self.instance_variables.each do |var|
-        hash[var.to_s.delete('@').to_sym] = self.instance_variable_get var
-      end
-      hash
-    end
-
-    def to_json
-      JSON.generate to_hash
-    end
-
-    def to_s
-      s = "#{self.class}("
-      self.to_hash.each do |k,v|
-        if v.is_a? String
-          s += "#{k}: \"#{v}\", "
-        else
-          s += "#{k}: #{v}, "
-        end
-      end
-      s.gsub /..$/, ')'
-    end
-    alias :inspect :to_s
-
-    def update(hash)
-      return self unless  hash.kind_of? Hash
-      hash.each do |k,v|
-        if self.respond_to?("#{k}=")
-          self.send "#{k}=", v
-        end
-      end
-      self
-    end
-
-    def save
-      File.write self.class.file_path(id), to_json
-    end
+  def save
+    File.write self.class.file_path(id), to_json
   end
+
+  def delete
+    self.class.bef_des_func.each do |func_name|
+      self.send func_name
+    end
+    File.delete self.class.file_path(id)
+  end
+  
+
+  def delete_all
+    #TODO delete object instances
+  end
+
+end
--- objects/user.rb
+++ objects/user.rb
@@ -4,6 +4,21 @@ class User < Base
    field :name, :string
    field :surname, :string
    field :age, :integer
+   field :email, :string
+   field :password, :string
    has_many :posts
+   before_destroy :delete_posts
+   before_destroy :on_destroy
+
+   def delete_posts
+     self.posts.each do |post|
+       post.delete
+     end
+   end
+
+   def on_destroy
+     puts "on users destroy #{self.id}"
+   end
+
 end
 
